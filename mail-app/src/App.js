0a81ab2c7e962060ce070ac8296ac5be557fd172
Huzaifa-code <huzaifaqureshi037@gmail.com>
2023-01-11 22:07:05 +0530

    created components folder

--- mail-app/src/App.js
+++ mail-app/src/App.js
@@ -1,7 +1,14 @@
+import Header from "./components/Header/Header";
+import Main from "./components/main/Main";
+import Footer from "./components/Footer/Footer"
+
 function App() {
 	return (
 		<>
 			<h1 className="font-bold text-lg justify-center">Mail App</h1>
+			<Header/>
+			<Main/>
+			<Footer/>
 		</>
 	);
 }
